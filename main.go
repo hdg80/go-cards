9d5aada7eb98cd9d260612610e7da2e87e2ad4ff
Heath George <hdgr34gtr@gmail.com>
2019-03-09 01:28:33 +1100

    slices and for loops

--- main.go
+++ main.go
@@ -5,9 +5,21 @@ import "fmt"
 func main() {
 
 	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
+	// card := newCard()
 	// only have to use colon on initial declaration of variable
-	card = "Five of Diamonds"
+	// card = "Five of Diamonds"
 
-	fmt.Println(card)
+	// Slice - variable length array
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	// Using for loop to iterate over slice
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
