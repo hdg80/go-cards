a448fabb2cf40f845aa4f12e633f242730f2c61a
Heath George <hdgr34gtr@gmail.com>
2019-03-08 20:14:07 +1100

    Updated file

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Card Game")
+func main() {
+
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	// only have to use colon on initial declaration of variable
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
 }
